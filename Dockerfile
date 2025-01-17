scott-8/smtp-gotify

commit b73876bfd51a7a0b16245fdc76a0853c84b3f389

Update Dockerfile
Newer Base Versions Used for Alpine and Go

Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.13-alpine3.10 AS builder
+FROM golang:1.22-alpine3.19 AS builder
 
 RUN apk add --no-cache git ca-certificates
 
@@ -9,7 +9,7 @@ COPY . .
 # The image should be built with
 # --build-arg SG_VERSION=`git describe --tags --always`
 ARG SG_VERSION
-RUN if [ ! -z "$SG_VERSION" ]; then sed -i "s/UNKNOWN_RELEASE/${SG_VERSION}/g" smtp-gotify.go; fi
+RUN if [ ! -z "$SG_VERSION" ]; then sed -i "s/UNKNOWN_RELEASE/${SG_VERSION}/>
 
 RUN CGO_ENABLED=0 GOOS=linux go build \
         -ldflags "-s -w" \
@@ -19,7 +19,8 @@ RUN CGO_ENABLED=0 GOOS=linux go build \
 
 
 
-FROM alpine:3.10
+FROM alpine:3.19
+
 
 RUN apk add --no-cache ca-certificates
 

commit cdf7e396aedad5a71c838c2f0e3c3cbfec0f00cf

fix Dockerfile bug introduced in b73876bfd51a7a0b16245fdc76a0853c84b3f389

Dockerfile
@@ -9,7 +9,7 @@ COPY . .
 # The image should be built with
 # --build-arg SG_VERSION=`git describe --tags --always`
 ARG SG_VERSION
-RUN if [ ! -z "$SG_VERSION" ]; then sed -i "s/UNKNOWN_RELEASE/${SG_VERSION}/>
+RUN if [ ! -z "$SG_VERSION" ]; then sed -i "s/UNKNOWN_RELEASE/${SG_VERSION}/g" smtp-gotify.go; fi
 
 RUN CGO_ENABLED=0 GOOS=linux go build \
         -ldflags "-s -w" \